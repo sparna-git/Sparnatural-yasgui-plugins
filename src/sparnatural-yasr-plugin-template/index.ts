d3baddf0529252d3cb1deaff01a441dcad0a73a9
Thomas Francart <thomas.francart@sparna.fr>
2024-04-22 16:38:34 +0200

    documentation on template plugin

--- src/sparnatural-yasr-plugin-template/index.ts
+++ src/sparnatural-yasr-plugin-template/index.ts
@@ -29,19 +29,49 @@ export class MyTestPlugin implements SparnaturalPlugin<PluginConfig> {
       return svgContainer;
 	}
 
+	/**
+	 * Cette méthode est celle qui affiche les résultats.
+	 * On peut écrire un bout de HTML dans this.yasr.resultsEl : this.yasr.resultsEl.appendChild(...);
+	 * 
+	 * @param persistentConfig 
+	 */
 	public draw(persistentConfig: PersistentConfig) {
 		console.log("Plugin drawing !");
+		var resultContainerEl = document.createElement("div");		
+		resultContainerEl.innerHTML = "Hello this is <strong>MyTestPlugin !!!</strong>";
+		
+		this.yasr.resultsEl.appendChild(resultContainerEl);
 	}
 
+	/**
+	 * Cette méthode est appelée pour savoir si le plugin peut afficher les résultats courants de la requête.
+	 * Elle renvoie un booléen true/false
+	 * Elle peut tester le tableau de résultat qui est dans "this.yasr.results"
+	 * 
+	 * Si le résultat de cette méthode est "false", l'onglet sera grisé
+	 * @returns 
+	 */
 	public canHandleResults() {
     	return !!this.yasr.results && this.yasr.results.getVariables() && this.yasr.results.getVariables().length > 0;
   	}
 
+	/**
+	 * Cette méthode est appelée quand la query Sparnatural est passée au plugin d'affichage.
+	 * Il faut garder la query dans une variable de classe pour y faire référence plus tard dans la méthode "draw"
+	 * 
+	 * @param sparnaturalQuery 
+	 */
 	public notifyQuery(sparnaturalQuery:ISparJson) {
 		console.log("received query");
 		console.log(sparnaturalQuery);
 	}
 
+	/**
+	 * Cette méthode est appelée quand la configuration Sparnatural est passée au plugin d'affichage.
+	 * Il faut garder la configuration dans une variable de classe pour y faire référence plus tard dans la méthode "draw"
+	 * 
+	 * @param specProvider 
+	 */
 	public notifyConfiguration(specProvider:any) {
 		console.log("received specification provider from Sparnatural");
 		console.log(specProvider);
