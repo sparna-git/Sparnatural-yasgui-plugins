2ca8d56114cd4f526c9e8707b503fb1901d14d23
Thomas Francart <thomas.francart@sparna.fr>
2024-06-24 17:17:25 +0200

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ export * from "./sparnatural-yasr-plugin-grid";
 export * from "./sparnatural-yasr-tablex-plugin";
 export * from "./sparnatural-yasr-htmltemplate-plugin";
 export * from "./sparnatural-yasr-map-plugin";
-export * from "./sparnatural-yasr-plugin-stats";
 
 export interface Yasr {
   setTabTitle(arg0: string): unknown;
